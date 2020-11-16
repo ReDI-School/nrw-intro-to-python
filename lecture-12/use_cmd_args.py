42b7c2ed07438c265bf82703cb2a5f513b28dad7
DeanEmmettSmith <dean.emmett.smith@accenture.com>
2020-11-16 21:49:08 +0100

    Lesson 12 completed.

--- lecture-12/use_cmd_args.py
+++ lecture-12/use_cmd_args.py
@@ -7,8 +7,9 @@ print(cmd_args)
 if len(cmd_args) == 2:
     print("Hello", cmd_args[1])
 
-if len(cmd_args) == 3:
+elif len(cmd_args) == 3:
     print("Hello", cmd_args[1], "and", cmd_args[2])
 
-if len(cmd_args) >= 4:
-    print("Hello "+ ", ".join(cmd_args[1:-2]) + ", " + cmd_args[-2] + " and " + cmd_args[-1])
+elif len(cmd_args) >= 4:
+    print("Hello " + ", ".join(cmd_args[1:-2]) + ", " + cmd_args[-2] + " and " + cmd_args[-1])
+
