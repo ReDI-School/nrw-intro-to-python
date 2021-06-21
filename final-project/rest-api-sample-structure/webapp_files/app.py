08a385baa0615b0ebccf0a07deb008f3a578b5b4
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2021-06-21 19:16:01 +0200

    Adding final project code

---
+++ final-project/rest-api-sample-structure/webapp_files/app.py
@@ -0,0 +1,21 @@
+﻿# This is going to be a book store
+# We would like to Create (Add), Checkout (Remove), Update and Read titles
+
+# This app uses the rather simple Flask framework
+from flask import Flask
+from flask import request
+import json
+app = Flask(__name__)
+
+# Routes define your API endpoints, if you will
+# @app.route() is called a Decorator - it "decorates" a function
+# This route is a dynamic route that can contain the ISBN of a book
+@app.route("/things", methods=['GET'])
+def get_things(optionalparametername=None):
+    response = app.response_class(
+        response='That works',
+        status=200,
+        mimetype='application/json'
+    )
+
+    return response
---
+++ final-project/rest-api-sample-structure/webapp_files/readme.md
@@ -0,0 +1,10 @@
+Place your web app files here - this is your root directory.
+
+Once you are done developing, be sure to update ../requirements.txt
+
+```bash
+virtualenv mywebapp
+source mywebapp/bin/activate
+pip freeze > requirements.txt
+deactivate
+```
