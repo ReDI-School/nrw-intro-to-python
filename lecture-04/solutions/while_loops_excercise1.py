cc48ff384be7d53445777c3dce6edc1c707d63c1
Henning Blunck <mail@henningblunck.de>
2021-09-22 17:44:37 +0200

    adjusted solution to square root excercise

--- lecture-04/solutions/while_loops_excercise1.py
+++ lecture-04/solutions/while_loops_excercise1.py
@@ -2,7 +2,7 @@ S = int(input('Number of of which you want to find the square root'))
 guess = float(input(f'Initial guess for square root of {S}'))
 
 next_guess = (guess + S/guess) / 2
-while abs(guess - next_guess) >= 0.001:
+while not (-0.001 <= guess - next_guess <= 0.001):
     print(next_guess)
     guess = next_guess
     next_guess = (guess + S/guess) / 2
